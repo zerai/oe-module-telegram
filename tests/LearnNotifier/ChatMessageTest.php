a729a65896dcee427210d5d146745179b001681a
zerai <teclaizerai@gmail.com>
2020-02-05 11:12:42 +0100

    Add some basic tests.

---
+++ tests/LearnNotifier/ChatMessageTest.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace OpenEMR\Modules\TelegramNotifier\Tests\LearnNotifier;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Notifier\Message\ChatMessage;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\Recipient\Recipient;
+
+class ChatMessageTest extends TestCase
+{
+    private const IRRELEVANT_MESSAGE = 'irrelevant message...';
+
+    /** @test */
+    public function can_be_created(): void
+    {
+        $message = new ChatMessage(self::IRRELEVANT_MESSAGE);
+
+        self::assertInstanceOf(ChatMessage::class, $message);
+        self::assertEquals(self::IRRELEVANT_MESSAGE, $message->getSubject());
+        self::assertNull($message->getOptions());
+    }
+
+    /** @test */
+    public function can_be_created_from_factory_method(): void
+    {
+        $notification = new Notification(self::IRRELEVANT_MESSAGE);
+        $recipient = new Recipient('joe.doe@example.com');
+        $transport = null;
+
+        $message = ChatMessage::fromNotification($notification, $recipient, $transport);
+
+        self::assertInstanceOf(ChatMessage::class, $message);
+        self::assertEquals(self::IRRELEVANT_MESSAGE, $message->getSubject());
+        self::assertNull($message->getOptions());
+        self::assertNull($message->getTransport());
+        self::assertNotNull($message->getNotification());
+        self::assertEquals(self::IRRELEVANT_MESSAGE, $message->getNotification()->getSubject());
+    }
+
+    /** @test */
+    public function can_change_the_message(): void
+    {
+        $message = new ChatMessage(self::IRRELEVANT_MESSAGE);
+
+        $message->subject('hi..!! '.self::IRRELEVANT_MESSAGE);
+
+        self::assertEquals('hi..!! '.self::IRRELEVANT_MESSAGE, $message->getSubject());
+    }
+}
---
+++ tests/Unit/PlaceholderTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace OpenEMR\Modules\TelegramNotifier\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceholderTest extends TestCase
+{
+    /** @test */
+    public function delete_me_iam_an_example(): void
+    {
+        $result = 1+1;
+
+        self::assertEquals(2, $result);
+    }
+}
